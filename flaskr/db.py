ba09de86d3a39f2c778584c0d788cf284d204ea3
Jono <jono.nicholas@hotmail.co.uk>
2020-10-30 18:47:21 +0000

    Changed db.execute to db.executescript

--- flaskr/db.py
+++ flaskr/db.py
@@ -28,7 +28,7 @@ def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
-        db.execute(f.read().decode('utf8'))
+        db.executescript(f.read().decode('utf8'))
 
     click.echo('Initialised the database.')
 
