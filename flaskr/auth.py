630c1f8ee18c03f06f4fb2334d0f2b2bc09ebff2
Jono <jono.nicholas@hotmail.co.uk>
2020-10-30 19:58:12 +0000

    Updated register method to POST, fixed login return and add login_req

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,7 +10,7 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'GET':
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         db = get_db()
@@ -56,7 +56,7 @@ def login():
             return redirect(url_for('index'))
 
         flash(error)
-        return render_template('auth/login.html')
+    return render_template('auth/login.html')
 
 
 @bp.before_app_request
@@ -68,3 +68,14 @@ def load_logged_in_user():
     else:
         g.user = get_db().execute('SELECT * FROM user WHERE id = ?',
                                   (user_id,)).fetchone()
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
