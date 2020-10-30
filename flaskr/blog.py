4d6b2df68c18c307a1edcbd48e80008c3241026f
Jono <jono.nicholas@hotmail.co.uk>
2020-10-30 21:51:47 +0000

    Fixed delete methods and query

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -70,12 +70,12 @@ def update(id=None):
     return render_template('blog/update.html', post=post)
 
 
-@bp.route('/<int:id>/delete', methods='POST')
+@bp.route('/<int:id>/delete', methods=['POST'])
 @login_required
 def delete(id):
     get_post(id)
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', id)
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
 
