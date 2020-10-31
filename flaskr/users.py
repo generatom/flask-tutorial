6b4989e3882be14eeb606e2f4f52e839390639d8
Jono <jono.nicholas@hotmail.co.uk>
2020-10-31 02:53:17 +0000

    List user functionality added for admin

--- flaskr/users.py
+++ flaskr/users.py
@@ -2,7 +2,17 @@ from flask import Blueprint, flash, g, render_template, request, url_for
 from flask import redirect
 from werkzeug.exceptions import abort
 from flaskr.db import get_db
-from flaskr.auth import login_required
+from flaskr.auth import admin_required
 
 
 bp = Blueprint('users', __name__, url_prefix='/users')
+
+
+@bp.route('/')
+@admin_required
+def list():
+    db = get_db()
+    users = db.execute(
+        'SELECT username, admin FROM user'
+    ).fetchall()
+    return render_template('users/list.html', users=users)
