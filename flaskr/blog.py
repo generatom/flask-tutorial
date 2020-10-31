1bbf5d25018b274953fbbe456b9dc18a08682c98
Jono <jono.nicholas@hotmail.co.uk>
2020-10-31 02:12:27 +0000

    Added in admin functionality

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -89,7 +89,8 @@ def get_post(id, check_author=True):
 
     if post is None:
         abort(404, f'Post {id} does not exist.')
-    if check_author and post['author_id'] != g.user['id']:
+    if check_author and (post['author_id'] != g.user['id'] and
+                         not g.user['admin']):
         abort(403)
 
     return post
