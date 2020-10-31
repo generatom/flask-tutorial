c1e14c6bb5d253bb17302a26b016a18f9ecf5caa
Jono <jono.nicholas@hotmail.co.uk>
2020-10-31 01:00:21 +0000

    Auto login after registration

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -28,7 +28,7 @@ def register():
             db.execute('INSERT INTO user (username, password) VALUES (?, ?)',
                        (username, generate_password_hash(password)))
             db.commit()
-            return redirect(url_for('auth.login'))
+            return login()
 
         flash(error)
 
